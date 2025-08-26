d153558b716f3c7293a0690725206ea7445026a0
Morris Allison III <morrisallison@users.noreply.github.com>
2025-08-26 06:56:01 -0700

    feat: add `refToPromise` utility

---
+++ .changeset/thin-dodos-live.md
@@ -0,0 +1,5 @@
+---
+"@knyt/artisan": minor
+---
+
+- Add `refToPromise` utility to convert a `Reference` to a `Promise`.
---
+++ packages/artisan/src/Reference/refToPromise.ts
@@ -0,0 +1,26 @@
+import { observableToPromise, type ObservablePromise } from "../Observable/mod";
+import type { Reference } from "./types";
+
+/**
+ * Converts a reference to a promise.
+ *
+ * @remarks
+ *
+ * The promise resolves when the reference updates with a value that satisfies
+ * the `shouldResolve` function.
+ *
+ * The promise rejects if the given abort signal is triggered or times out.
+ *
+ * By default, the promise resolves if the reference contains a truthy value.
+ *
+ * @public
+ */
+export function refToPromise<T>(
+  reference: Reference.Readonly<T>,
+  options?: ObservablePromise.Options<T>,
+): Promise<T> {
+  return observableToPromise(reference, {
+    shouldResolve: Boolean,
+    ...options,
+  });
+}
