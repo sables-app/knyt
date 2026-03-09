75279d08638599a41ce7fdd9dc30c85f93147c8d
Morris Allison III <morrisallison@users.noreply.github.com>
2026-03-09 09:03:20 -0700

    fix: correct import path

--- packages/clerk/src/main.ts
+++ packages/clerk/src/main.ts
@@ -1,6 +1,6 @@
 export * from "./action/mod.ts";
 export * from "./createAccessor.ts";
-export * from "./logDispatch"
+export * from "./logDispatch.ts"
 export * from "./reduce/mod.ts";
 export * from "./select/mod.ts";
 export * from "./Store.ts";
